7012794c5af436c009b1e235d151dc9f78155d9f
Sina Mahmoodi <itz.s1na@gmail.com>
2020-12-16 15:21:29 +0100

    tree: add Hash method

--- tree.go
+++ tree.go
@@ -15,26 +15,26 @@ func NewTree() *Node {
 	return &Node{left: nil, right: nil, value: nil}
 }
 
-func TreeFromChunks(chunks [][]byte) *Node {
-	numLeaves := int(nextPowerOfTwo(uint64(len(chunks))))
-	numEmpty := numLeaves - len(chunks)
-	numNodes := numLeaves*2 - 1
+func TreeFromChunks(chunks [][]byte) (*Node, error) {
+	numLeaves := len(chunks)
+	if !isPowerOfTwo(numLeaves) {
+		return nil, errors.New("Number of leaves should be a power of 2")
+	}
 
+	numNodes := numLeaves*2 - 1
 	nodes := make([]*Node, numNodes)
 	for i := numNodes; i > 0; i-- {
-		// It's a leaf
+		// Is a leaf
 		if i > numNodes-numLeaves {
-			val := []byte(nil)
-			if i <= numNodes-numEmpty {
-				val = chunks[i-numLeaves]
-			}
+			val := chunks[i-numLeaves]
 			nodes[i-1] = &Node{left: nil, right: nil, value: val}
 		} else {
+			// Is a branch node
 			nodes[i-1] = &Node{left: nodes[(i*2)-1], right: nodes[(i*2+1)-1], value: nil}
 		}
 	}
 
-	return nodes[0]
+	return nodes[0], nil
 }
 
 func (n *Node) Get(index int) (*Node, error) {
@@ -52,4 +52,25 @@ func (n *Node) Get(index int) (*Node, error) {
 	}
 
 	return cur, nil
+}
+
+func (n *Node) Hash() []byte {
+	// TODO: handle special cases: empty root, one non-empty node
+	return hashNode(n)
+}
+
+func hashNode(n *Node) []byte {
+	// Leaf
+	if n.left == nil && n.right == nil {
+		return n.value
+	}
+	// Only one child
+	if n.left == nil || n.right == nil {
+		panic("Tree incomplete")
+	}
+	return hashFn(append(hashNode(n.left), hashNode(n.right)...))
+}
+
+func isPowerOfTwo(n int) bool {
+	return (n & (n - 1)) == 0
 }
--- tree_test.go
+++ tree_test.go
@@ -2,6 +2,7 @@ package ssz
 
 import (
 	"bytes"
+	"encoding/hex"
 	"testing"
 )
 
@@ -10,22 +11,45 @@ func TestTreeFromChunks(t *testing.T) {
 		{0x01, 0x01},
 		{0x02, 0x02},
 		{0x03, 0x03},
+		{0x00, 0x00},
 	}
 
-	r := TreeFromChunks(chunks)
+	r, err := TreeFromChunks(chunks)
+	if err != nil {
+		t.Errorf("Failed to construct tree: %v\n", err)
+	}
 	for i := 4; i < 8; i++ {
 		l, err := r.Get(i)
 		if err != nil {
 			t.Errorf("Failed getting leaf: %v\n", err)
 		}
-		if i > 6 {
-			if l.value != nil {
-				t.Errorf("Incorrect leaf at index %d. Leaf should be empty\n", i)
-			}
-		} else {
-			if !bytes.Equal(l.value, chunks[i-4]) {
-				t.Errorf("Incorrect leaf at index %d\n", i)
-			}
+		if !bytes.Equal(l.value, chunks[i-4]) {
+			t.Errorf("Incorrect leaf at index %d\n", i)
 		}
+	}
+}
+
+func TestHashTree(t *testing.T) {
+	expectedRootHex := "6621edd5d039d27d1ced186d57691a04903ac79b389187c2d453b5d3cd65180e"
+	expectedRoot, err := hex.DecodeString(expectedRootHex)
+	if err != nil {
+		t.Errorf("Failed to decode hex string\n")
+	}
+
+	chunks := [][]byte{
+		{0x01, 0x01},
+		{0x02, 0x02},
+		{0x03, 0x03},
+		{0x00, 0x00},
+	}
+
+	r, err := TreeFromChunks(chunks)
+	if err != nil {
+		t.Errorf("Failed to construct tree: %v\n", err)
+	}
+
+	h := r.Hash()
+	if !bytes.Equal(h, expectedRoot) {
+		t.Errorf("Computed hash is incorrect. Expected %s, got %s\n", expectedRootHex, hex.EncodeToString(h))
 	}
 }
