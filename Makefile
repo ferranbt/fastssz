b0c3c86fe9c4888b66d33631e472e30810d8af26
Sina Mahmoodi <itz.s1na@gmail.com>
2021-01-20 11:55:15 +0100

    Fix changes to spectests

--- Makefile
+++ Makefile
@@ -4,7 +4,7 @@ build-spec-tests:
 	go run sszgen/*.go --path ./spectests/structs.go --include ./spectests/external,./spectests/external2
 
 build-spec-tests-tree:
-	go run sszgen/*.go --path ./spectests/structs.go --objs AttestationData
+	go run sszgen/*.go --path ./spectests/structs.go --objs AttestationData --experimental
 
 .PHONY:
 get-spec-tests:
--- spectests/structs.go
+++ spectests/structs.go
@@ -2,6 +2,7 @@ package spectests
 
 import (
 	"github.com/ferranbt/fastssz/spectests/external"
+	external2Alias "github.com/ferranbt/fastssz/spectests/external2"
 )
 
 type AggregateAndProof struct {
@@ -11,8 +12,8 @@ type AggregateAndProof struct {
 }
 
 type Checkpoint struct {
-	Epoch uint64 `json:"epoch"`
-	Root  []byte `json:"root" ssz-size:"32"`
+	Epoch external2Alias.EpochAlias `json:"epoch"`
+	Root  []byte                    `json:"root" ssz-size:"32"`
 }
 
 type Slot uint64 // alias from the same package
