7de01f5ec5a42b38ce041e706c2640b451898c91
claravanstaden <Cats 4 life!>
2023-02-06 20:29:39 +0200

    Check testcases directory only for testcases git status. Check if removing go mod vendor works.

--- scripts/ci-validate-testcases.sh
+++ scripts/ci-validate-testcases.sh
@@ -5,8 +5,7 @@ make generate-testcases
 
 # check differences
 cd sszgen/testcases
-git status --porcelain
-if [[ `git status --porcelain` ]]; then
+if [[ `git status --porcelain .` ]]; then
   echo "Testcases have not been generated."
   exit 1
 else
