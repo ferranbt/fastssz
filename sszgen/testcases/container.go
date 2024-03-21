53db667a0705361f59c1c89c41385836250242af
Ferran Borreguero <ferran.borreguero@gmail.com>
2024-03-21 12:02:00 +0000

    Split in two containers

--- sszgen/testcases/container.go
+++ sszgen/testcases/container.go
@@ -3,6 +3,9 @@ package testcases
 //go:generate go run ../main.go --path container.go
 
 type Vec struct {
-	Values  []uint64 `ssz-size:"6"`
+	Values []uint64 `ssz-size:"6"`
+}
+
+type Vec2 struct {
 	Values2 []uint32 `ssz-max:"100"`
 }
